5d966694ea939b4438188626e0f4caaf2b4a5cb6
Brandon Yu <yubrandon@yubrandon.yubrandon.org>
2023-12-22 20:00:20 -0800

    added hover visuals

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var op,x,y;
+var scan;
+var compute = false;
 
 
 function createButtons() {
     const screen = document.querySelector('.screen');
 
-    const btn = document.querySelectorAll('.value');
-    for(let i = 0; i < btn.length;i++) {
-        btn[i].addEventListener('click',function(e) {
+    const btn = document.querySelectorAll('button');
+    for(let i = 0; i < btn.length; i++) {
+        let computedStyle = window.getComputedStyle(btn[i]);
+        btn[i].addEventListener('mouseover', () => {
+            btn[i].style.backgroundColor = computedStyle.getPropertyValue('--hover-color');            
+        });
+        btn[i].addEventListener('mouseout', () => {
+            btn[i].style.backgroundColor = computedStyle.getPropertyValue('--bg-color');
+        })
+    }
+
+    const vals = document.querySelectorAll('.value');
+    for(let i = 0; i < vals.length;i++) {
+        vals[i].addEventListener('click',function(e) {
             //console.log(e.target.innerText);
             display(e.target.innerText);
         });
@@ -21,12 +34,44 @@ function createButtons() {
         else if(screen.innerText.length == 11) screen.innerText = "-" + screen.innerText.slice(0,10);
         else if (screen.innerText != "0" )screen.innerText = "-" + screen.innerText;
     });
+
+    const percent = document.querySelector('.percent');
+    percent.addEventListener('click',(getPercent));
+
+    const operators = document.querySelectorAll('.operator');
+    for(let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', function(e) {
+        console.log(e.target);
+        if(screen.innerText[0] == '-') x = 0 - parseInt(screen.innerText);
+        else x = parseInt(screen.innerText);
+        compute = true;
+    });
+    }
+    
+
+    const equal = document.querySelector('.equal');
+    equal.addEventListener('click',() => {
+        screen.innerText = operate(op,x,y);
+        compute = false;
+    });
 }
 createButtons();
 
 
 function operate(op,a,b) {
-
+    /*
+    + : 0
+    - : 1
+    * : 2
+    / : 3
+    */
+    switch(op) {
+        case 0: return a + b;
+        case 1: return a - b;
+        case 2: return a * b;
+        case 3: return a/b;
+        default: return a;
+    }
 }
 function display(n) {
     const screen = document.querySelector('.screen');
@@ -40,6 +85,12 @@ function display(n) {
 function clear() {
     const screen = document.querySelector('.screen');
     screen.innerText = "0";
+}
+
+function getPercent() {
+    const screen = document.querySelector('.screen');
+    screen.innerText = parseInt(screen.innerText)/100;
+
 }
 
 /*
